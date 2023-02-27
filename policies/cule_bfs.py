f7c6c8cbd48169d801423b64a823769c6cfe114f
Assaf Hallak <ahallak@nvidia.com>
2023-02-27 17:26:17 +0200

    + Added a merge of bfs_orig and bfs_with_width (require testing)

--- policies/cule_bfs.py
+++ policies/cule_bfs.py
@@ -294,4 +294,114 @@ class CuleBFS():
         env.lives[:env.size()] = lives[env_indices]
 
     def bfs_merged(self, state, tree_depth):
-        pass
+        state_clone = state.clone().detach()
+
+        max_width = self.max_width
+
+        cpu_env = self.cpu_env
+        gpu_env = self.gpu_env
+        step_env = self.step_env
+
+        first_action = th.arange(0, cpu_env.action_space.n, device=cpu_env.device).unsqueeze(1) if max_width != -1 else None
+
+        # Set device environment root state before calling step function
+        cpu_env.states[0] = step_env.states[0]
+        cpu_env.ram[0] = step_env.ram[0]
+        cpu_env.frame_states[0] = step_env.frame_states[0]
+
+        # Zero out all buffers before calling any environment functions
+        cpu_env.rewards.zero_()
+        cpu_env.observations1.zero_()
+        cpu_env.observations2.zero_()
+        cpu_env.done.zero_()
+
+        # Make sure all actions in the backend are completed
+        # Be careful making calls to pytorch functions between cule synchronization calls
+        if gpu_env.is_cuda:
+            gpu_env.sync_other_stream()
+        # Create a default depth_env pointing to the CPU backend
+        depth_env = cpu_env
+        depth_actions_initial = self.cpu_actions
+        num_envs = 1
+        relevant_env = depth_env if tree_depth > 0 else step_env
+        for depth in range(tree_depth):
+            # By level 3 there should be enough states to warrant moving to the GPU.
+            # We do this by copying all of the relevant state information between the
+            # backend GPU and CPU instances.
+            if depth == self.crossover_level:
+                self.copy_envs(cpu_env, gpu_env)
+                depth_env = gpu_env
+                relevant_env = depth_env if tree_depth > 0 else step_env
+                depth_actions_initial = self.gpu_actions
+
+            # Compute the number of environments at the current depth
+            if max_width == -1:
+                num_envs = self.min_actions_size ** (depth + 1)
+                # TODO: Are the two expands different?
+                depth_env.expand(num_envs)
+                depth_actions = depth_actions_initial.repeat(self.min_actions_size ** depth)
+            else:
+                min(self.min_actions_size ** (depth + 1), max_width * self.min_actions_size)
+                self.new_expand(depth_env, num_envs)
+                if depth != 0:
+                    first_action = first_action.repeat(1, cpu_env.action_space.n).view(-1, 1)
+                depth_actions = depth_actions_initial.repeat(max_width)
+            # Loop over the number of frameskips
+            for frame in range(depth_env.frameskip):
+                # Execute backend call to the C++ step function with environment data
+                super(AtariEnv, depth_env).step(depth_env.fire_reset and depth_env.is_training, False,
+                                                depth_actions.data_ptr(), 0, depth_env.done.data_ptr(), 0)
+                # Update the reward, done, and lives flags
+                depth_env.get_data(depth_env.episodic_life, self.gamma ** depth, depth_env.done.data_ptr(),
+                                   depth_env.rewards.data_ptr(), depth_env.lives.data_ptr())
+
+                # To properly compute the output observations we need the last frame AND the second to last frame.
+                # On the second to last step we need to update the frame buffers
+                if frame == (depth_env.frameskip - 2):
+                    depth_env.generate_frames(depth_env.rescale, False, depth_env.num_channels,
+                                              depth_env.observations2[:num_envs].data_ptr())
+                if frame == (depth_env.frameskip - 1):
+                    depth_env.generate_frames(depth_env.rescale, False, depth_env.num_channels,
+                                              depth_env.observations1[:num_envs].data_ptr())
+            new_obs = th.max(depth_env.observations1[:num_envs], depth_env.observations2[:num_envs])
+            # frame stacking
+            new_obs = new_obs.squeeze(dim=-1).unsqueeze(dim=1).to(self.device)
+            state_clone = self.replicate_state(state_clone)
+            state_clone = th.cat((state_clone[:, 1: self.n_frame_stack, :, :], new_obs), dim=1)
+            th.cuda.synchronize()
+            # shrink the number of envs to max_width before expanding them again
+            if max_width != -1 and depth != tree_depth - 1 and num_envs > max_width:
+                leaves_vals = self.compute_val_func(state_clone)[0].max(dim=1).values
+                # TODO: compute cumulative clipped reward inside the depth loop
+                depth_rewards = th.sign(depth_env.rewards[:num_envs]) if self.clip_reward else depth_env.rewards[
+                                                                                               :num_envs]
+                pi_logit = depth_rewards + self.gamma ** (depth + 1) * leaves_vals.to(depth_env.device)
+                try:
+                    top_indexes = th.multinomial(th.softmax(th.clip(pi_logit, -1e6, 1e6), 0), max_width)
+                except:
+                    print("Bug in pi_logit", pi_logit)
+                    top_indexes = th.argsort(pi_logit, descending=True)[:max_width]
+                first_action = first_action[top_indexes]
+                state_clone = state_clone[top_indexes, :]
+                depth_env.rewards[:max_width] = depth_env.rewards[top_indexes]
+                depth_env.observations1[:max_width] = depth_env.observations1[top_indexes]
+                depth_env.observations2[:max_width] = depth_env.observations2[top_indexes]
+                depth_env.done[:max_width] = depth_env.done[top_indexes]
+                depth_env.states[:max_width, :] = depth_env.states[top_indexes, :]
+                depth_env.ram[:max_width] = depth_env.ram[top_indexes]
+                depth_env.frame_states[:max_width] = depth_env.frame_states[top_indexes]
+                depth_env.lives[:max_width] = depth_env.lives[top_indexes]
+                depth_env.set_size(max_width)
+        # Make sure all actions in the backend are completed
+        if depth_env.is_cuda:
+            depth_env.sync_this_stream()
+            th.cuda.current_stream().synchronize()
+        # Waits for everything to finish running
+        th.cuda.synchronize()
+
+        rewards = relevant_env.rewards[:num_envs].to(gpu_env.device)
+        if self.clip_reward:
+            rewards = th.sign(rewards)
+        cpu_env.set_size(1)
+        gpu_env.set_size(1)
+        return state_clone, rewards, first_action
